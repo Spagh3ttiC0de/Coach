2bebf65a72906999ab17792101fab11af82d9fca
Spagh3ttiC0de <60497271+Spagh3ttiC0de@users.noreply.github.com>
2025-08-30 22:40:42 +0200

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,13 +44,21 @@ namespace Coach
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnInnocent = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.radioSteam = new System.Windows.Forms.RadioButton();
+            this.radioEpic = new System.Windows.Forms.RadioButton();
+            this.radioGOG = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnHackerTool
             // 
-            this.btnHackerTool.Location = new System.Drawing.Point(7, 96);
+            this.btnHackerTool.Location = new System.Drawing.Point(7, 48);
             this.btnHackerTool.Name = "btnHackerTool";
             this.btnHackerTool.Size = new System.Drawing.Size(215, 23);
             this.btnHackerTool.TabIndex = 4;
@@ -60,7 +68,7 @@ namespace Coach
             // 
             // BtnTorch
             // 
-            this.BtnTorch.Location = new System.Drawing.Point(7, 137);
+            this.BtnTorch.Location = new System.Drawing.Point(7, 79);
             this.BtnTorch.Name = "BtnTorch";
             this.BtnTorch.Size = new System.Drawing.Size(215, 23);
             this.BtnTorch.TabIndex = 19;
@@ -78,7 +86,7 @@ namespace Coach
             "Medium",
             "Hard",
             "Nightmare"});
-            this.comboBox1.Location = new System.Drawing.Point(7, 58);
+            this.comboBox1.Location = new System.Drawing.Point(7, 21);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(215, 21);
             this.comboBox1.TabIndex = 20;
@@ -156,7 +164,7 @@ namespace Coach
             // 
             // btnEnableGasMask
             // 
-            this.btnEnableGasMask.Location = new System.Drawing.Point(7, 176);
+            this.btnEnableGasMask.Location = new System.Drawing.Point(6, 108);
             this.btnEnableGasMask.Name = "btnEnableGasMask";
             this.btnEnableGasMask.Size = new System.Drawing.Size(215, 23);
             this.btnEnableGasMask.TabIndex = 28;
@@ -184,12 +192,12 @@ namespace Coach
             this.groupBox1.Controls.Add(this.btnFaceHugger);
             this.groupBox1.Controls.Add(this.btnCivilian);
             this.groupBox1.Controls.Add(this.btnRiotGuards);
-            this.groupBox1.Location = new System.Drawing.Point(13, 231);
+            this.groupBox1.Location = new System.Drawing.Point(258, 72);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(229, 336);
             this.groupBox1.TabIndex = 30;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "AI";
+            this.groupBox1.Text = "NPC AI:";
             // 
             // btnInnocent
             // 
@@ -203,33 +211,108 @@ namespace Coach
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.btnDisableIntroMovies);
             this.groupBox2.Controls.Add(this.btnHackerTool);
             this.groupBox2.Controls.Add(this.BtnTorch);
             this.groupBox2.Controls.Add(this.btnEnableGasMask);
             this.groupBox2.Controls.Add(this.comboBox1);
             this.groupBox2.Location = new System.Drawing.Point(13, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(228, 213);
+            this.groupBox2.Size = new System.Drawing.Size(228, 142);
             this.groupBox2.TabIndex = 31;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Other";
+            this.groupBox2.Text = "Memory Hacks:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnDisableIntroMovies);
+            this.groupBox3.Location = new System.Drawing.Point(258, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(228, 54);
+            this.groupBox3.TabIndex = 32;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Other settings:";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.radioGOG);
+            this.groupBox4.Controls.Add(this.radioEpic);
+            this.groupBox4.Controls.Add(this.radioSteam);
+            this.groupBox4.Location = new System.Drawing.Point(13, 160);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(228, 95);
+            this.groupBox4.TabIndex = 33;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Override game version:";
+            // 
+            // radioSteam
+            // 
+            this.radioSteam.AutoSize = true;
+            this.radioSteam.Location = new System.Drawing.Point(7, 20);
+            this.radioSteam.Name = "radioSteam";
+            this.radioSteam.Size = new System.Drawing.Size(55, 17);
+            this.radioSteam.TabIndex = 0;
+            this.radioSteam.TabStop = true;
+            this.radioSteam.Text = "Steam";
+            this.radioSteam.UseVisualStyleBackColor = true;
+            this.radioSteam.CheckedChanged += new System.EventHandler(this.radioSteam_CheckedChanged);
+            // 
+            // radioEpic
+            // 
+            this.radioEpic.AutoSize = true;
+            this.radioEpic.Location = new System.Drawing.Point(7, 43);
+            this.radioEpic.Name = "radioEpic";
+            this.radioEpic.Size = new System.Drawing.Size(46, 17);
+            this.radioEpic.TabIndex = 1;
+            this.radioEpic.TabStop = true;
+            this.radioEpic.Text = "Epic";
+            this.radioEpic.UseVisualStyleBackColor = true;
+            this.radioEpic.CheckedChanged += new System.EventHandler(this.radioSteam_CheckedChanged);
+            // 
+            // radioGOG
+            // 
+            this.radioGOG.AutoSize = true;
+            this.radioGOG.Location = new System.Drawing.Point(7, 66);
+            this.radioGOG.Name = "radioGOG";
+            this.radioGOG.Size = new System.Drawing.Size(49, 17);
+            this.radioGOG.TabIndex = 2;
+            this.radioGOG.TabStop = true;
+            this.radioGOG.Text = "GOG";
+            this.radioGOG.UseVisualStyleBackColor = true;
+            this.radioGOG.CheckedChanged += new System.EventHandler(this.radioSteam_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(16, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "            ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(256, 575);
+            this.ClientSize = new System.Drawing.Size(493, 422);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "AI v1.5.2";
+            this.Text = "AI v1.6.0";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -250,6 +333,12 @@ namespace Coach
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button btnInnocent;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton radioGOG;
+        private System.Windows.Forms.RadioButton radioEpic;
+        private System.Windows.Forms.RadioButton radioSteam;
+        private System.Windows.Forms.Label label1;
     }
 }
 
